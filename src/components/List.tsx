2645e57d44976a63b11bbb6c3b55cd97ab909638
dibkb <dibas9110@gmail.com>
2023-04-24 23:39:48 +0530

    view insights button

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,10 +8,15 @@ const List: React.FC = () => {
       <div className="flex flex-col gap-4">
         {Items.map((element) => (
           <div>
-            <h1 className="mb-4 font-medium text-sky-600 flex items-center gap-3 cursor-pointer hover:underline">
-              {element.title}
-              <ArrowUpRightIcon className="h-5 w-5" />
-            </h1>
+            <span className="flex justify-between items-start">
+              <h1 className="mb-4 font-medium text-sky-600 flex items-center gap-3 cursor-pointer hover:underline">
+                {element.title}
+                <ArrowUpRightIcon className="h-5 w-5" />
+              </h1>
+              <span className="text-xs border border-stone-600 p-1 rounded-md cursor-pointer font-medium text-stone-600">
+                View Insights
+              </span>
+            </span>
             <p className="text-sm">{element.content}</p>
           </div>
         ))}
