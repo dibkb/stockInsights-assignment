c2b0cde965acd79f12a701325926079413d11d86
dibkb <dibas9110@gmail.com>
2023-04-25 10:23:35 +0530

    clear all button

--- src/components/FilteredItem.tsx
+++ src/components/FilteredItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { stockOptions, timeOptions } from "../data/data";
 import { XMarkIcon } from "@heroicons/react/20/solid";
+import { TrashIcon } from "@heroicons/react/24/outline";
 const FilteredItem: React.FC<FilteredItem> = ({
   timeSelected,
   stockSelected,
@@ -8,7 +9,7 @@ const FilteredItem: React.FC<FilteredItem> = ({
   removeStockItemHandler,
 }) => {
   return (
-    <div className="flex gap-3 mt-8">
+    <div className="flex gap-3 mt-8 select-none flex-wrap items-center">
       {timeSelected.map((element) => (
         <span
           onClick={() => removeTimeItemHandler(element)}
@@ -30,6 +31,14 @@ const FilteredItem: React.FC<FilteredItem> = ({
           <p>{element.value}</p>
         </span>
       ))}
+      {timeSelected.length || stockSelected.length ? (
+        <span className="text-sm font-medium px-4 py-2 rounded-full bg-red-500 text-white flex gap-1 items-center cursor-pointer hover:bg-red-700">
+          <TrashIcon className="w-5 h-5" />
+          <p className="whitespace-nowrap">Clear all</p>
+        </span>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
