5f6d2ca571069f631c4bf987207799466e758cda
dibkb <dibas9110@gmail.com>
2023-05-02 13:26:54 +0530

    company filter

--- src/components/Filtermodal.tsx
+++ src/components/Filtermodal.tsx
@@ -1,9 +1,15 @@
+import { ChevronUpIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 const Filtermodal: React.FC = () => {
   return (
-    <div className="absolute bg-white border top-[120%] left-0 p-2 shadow-md">
-      Filtermodal
+    <div className="absolute bg-white border top-[120%] left-0 p-4 shadow-md rounded-md">
+      <main className="flex whitespace-nowrap gap-9">
+        <h3 className="font-medium">Company :</h3>
+        <h4 className="flex items-center gap-9 border-b pb-1">
+          All Companies <ChevronUpIcon className="w-4 h-4 text-gray-600" />
+        </h4>
+      </main>
     </div>
   );
 };
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout: React.FC = () => {
   return (
-    <div>
+    <div className="text-gray-800">
       <Navbar />
       <div className="sm:container max-w-[90%] mx-auto">
         <Outlet />
